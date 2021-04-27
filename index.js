84839bdb99578e4eedf107d9f6adc4f110560eda
danjudex <dan.judex@gmail.com>
2021-04-27 16:18:33 +0000

    timeout

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const fs = require("fs/promises");
 
+const timeout = (ms) =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+      console.log("TIMER");
+    }, ms);
+  });
+
 const main = async () => {
+  setInterval(() => {
+    console.log("TIMER");
+  }, 1000);
+
+  await timeout(5000);
   const json = await fs.readFile("sample.json");
   console.log(json);
 };

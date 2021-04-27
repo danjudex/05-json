3f83e4e2bc564453799dea823a295144731c0ab7
danjudex <dan.judex@gmail.com>
2021-04-27 16:37:38 +0000

    log

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 const fs = require("fs/promises");
 
-const timeout = (ms) =>
-  new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve();
-      console.log("TIMER");
-    }, ms);
-  });
+// const timeout = (ms) =>
+//   new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve();
+//       console.log("TIMER");
+//     }, ms);
+//   });
 
 const main = async () => {
-  setInterval(() => {
-    console.log("TIMER");
-  }, 1000);
+  const fileBuffer = await fs.readFile("sample.json");
+  const obj = JSON.parse(fileBuffer.toString());
 
-  await timeout(5000);
-  const json = await fs.readFile("sample.json");
-  console.log(json);
+  console.log(obj.firstName);
+  console.log(obj.address.city);
+  console.log(obj.phoneNumbers[0].number);
 };
 
 main();
-
-console.log("END");
